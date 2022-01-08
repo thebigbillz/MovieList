ad1f3bbc947acba99a25bde76c3d97d63cb484c8
thebigbillz <adenugbafavour@gmail.com>
2022-01-08 18:22:09 +0100

    Removed Search Icon

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { useSearchContext } from "../context/SearchContext";
 import "./SearchBar.scss";
-import SearchIcon from "@mui/icons-material/Search";
 
 const SearchBar = () => {
   const { search, setSearch } = useSearchContext();
@@ -13,14 +12,6 @@ const SearchBar = () => {
         value={search}
         onChange={(e) => {
           setSearch(e.target.value);
-        }}
-      />
-      <SearchIcon
-        style={{
-          color: "whitesmoke",
-          borderBottom: "2px solid rgba(255, 25, 25, 0.801)",
-          fontSize: "2.3rem",
-          paddingBottom: "0.5rem",
         }}
       />
     </div>
